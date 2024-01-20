f69ee0fdd799f8f284d3d44cb6555fd7b6c1d3e0
Phil Gumbley <philip.gumbley@gmail.com>
2024-01-19 22:55:15 -0500

    Added basic filter by fruit trees (grouped) and in season.

--- treefinder/views.py
+++ treefinder/views.py
@@ -3,6 +3,7 @@ from .models import Tree
 from django.core.serializers import serialize
 from django.http import JsonResponse
 import json
+from datetime import datetime
 
 def home(request):
     trees = serialize('json', Tree.objects.all())
@@ -13,13 +14,34 @@ def get_trees_within_bounds(request):
     ne_lng = request.GET.get('neLng')
     sw_lat = request.GET.get('swLat')
     sw_lng = request.GET.get('swLng')
+    name = request.GET.get('name')
+    in_season = request.GET.get('inSeason') == 'true'
+
+    # Define fruit groupings
+    fruit_groupings = {
+        'Apple': ['Apple', 'Apple, common'],
+        'Apricot': ['Apricot'],
+        'Cherry': ['Cherry', 'Cherry, black', 'Cherry, sweet'],
+        'Mulberry': ['Mulberry', 'Mulberry, red', 'Mulberry, white', 'Mulberry, white weeping'],
+        'Peach': ['Peach'],
+        'Pear': ['Pear'],
+        'Plum': ['Plum', 'Plum, Canada'],
+        'Serviceberry': [ 'Serviceberry', 'Serviceberry Robin hill', 'Serviceberry, smooth'],
+    }
 
     trees = Tree.objects.filter(
-        latitude__lte=ne_lat,
-        latitude__gte=sw_lat,
-        longitude__lte=ne_lng,
-        longitude__gte=sw_lng
+        latitude__lte=ne_lat, latitude__gte=sw_lat,
+        longitude__lte=ne_lng, longitude__gte=sw_lng
     )
+
+    if name and name != 'all':
+        specific_fruits = fruit_groupings.get(name, [name])
+        trees = trees.filter(name__in=specific_fruits)
+    
+    if in_season:
+        today = datetime.now().timetuple().tm_yday
+        trees = trees.filter(fruiting_start_date__lte=today, fruiting_end_date__gte=today)
+
     trees_data = serialize('json', trees)
-    trees_list = json.loads(trees_data)  # Parse the JSON string back into a Python object
-    return JsonResponse(trees_list, safe=False)  # Pass the Python object to JsonResponse
+    trees_list = json.loads(trees_data)
+    return JsonResponse(trees_list, safe=False)
