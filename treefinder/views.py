3c6019bd6c766fac7ed0acf7983f1e8517a6a75c
Phil Gumbley <philip.gumbley@gmail.com>
2024-03-24 22:49:22 -0400

    Add slider bar for date and tree display

--- treefinder/views.py
+++ treefinder/views.py
@@ -3,7 +3,6 @@ from .models import Tree
 from django.core.serializers import serialize
 from django.http import JsonResponse
 import json
-from datetime import datetime
 
 def home(request):
     trees = serialize('json', Tree.objects.all())
@@ -15,32 +14,32 @@ def get_trees_within_bounds(request):
     sw_lat = request.GET.get('swLat')
     sw_lng = request.GET.get('swLng')
     name = request.GET.get('name')
-    in_season = request.GET.get('inSeason') == 'true'
+    day_of_year = request.GET.get('dayOfYear')
 
-    # Define fruit groupings
-    fruit_groupings = {
-        'Apple': ['Apple', 'Apple, common'],
-        'Apricot': ['Apricot'],
-        'Cherry': ['Cherry', 'Cherry, black', 'Cherry, sweet'],
-        'Mulberry': ['Mulberry', 'Mulberry, red', 'Mulberry, white', 'Mulberry, white weeping'],
-        'Peach': ['Peach'],
-        'Pear': ['Pear'],
-        'Plum': ['Plum', 'Plum, Canada'],
-        'Serviceberry': [ 'Serviceberry', 'Serviceberry Robin hill', 'Serviceberry, smooth'],
-    }
-
+    # Initialize the trees queryset
     trees = Tree.objects.filter(
         latitude__lte=ne_lat, latitude__gte=sw_lat,
         longitude__lte=ne_lng, longitude__gte=sw_lng
     )
 
     if name and name != 'all':
+        # Define fruit groupings
+        fruit_groupings = {
+            'Apple': ['Apple', 'Apple, common'],
+            'Apricot': ['Apricot'],
+            'Cherry': ['Cherry', 'Cherry, black', 'Cherry, sweet'],
+            'Mulberry': ['Mulberry', 'Mulberry, red', 'Mulberry, white', 'Mulberry, white weeping'],
+            'Peach': ['Peach'],
+            'Pear': ['Pear'],
+            'Plum': ['Plum', 'Plum, Canada'],
+            'Serviceberry': ['Serviceberry', 'Serviceberry Robin hill', 'Serviceberry, smooth'],
+        }
         specific_fruits = fruit_groupings.get(name, [name])
         trees = trees.filter(name__in=specific_fruits)
-    
-    if in_season:
-        today = datetime.now().timetuple().tm_yday
-        trees = trees.filter(fruiting_start_day__lte=today, fruiting_end_day__gte=today)
+
+    if day_of_year:
+        day_of_year = int(day_of_year)
+        trees = trees.filter(fruiting_start_day__lte=day_of_year, fruiting_end_day__gte=day_of_year)
 
     trees_data = serialize('json', trees)
     trees_list = json.loads(trees_data)
