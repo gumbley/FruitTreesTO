e8c605401ef8132e3b5116788709444bd9f7de4e
Phil Gumbley <philip.gumbley@gmail.com>
2024-05-24 21:24:25 -0400

    Prepare for GitHub Pages deployment

--- treefinder/urls.py
+++ treefinder/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from .views import get_trees_within_bounds
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('api/trees', get_trees_within_bounds, name='get_trees_within_bounds'),
 ]
